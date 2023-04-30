40904d1fac9af5abe0dc0b1e5accb0054a13579a
HerRoHee <118667834+HerRoHee@users.noreply.github.com>
2023-04-30 21:43:31 +0200

    Add files via upload

---
+++ First year/I Semester/PR I/Materijali/Uvod/tipovi podataka, varijable, cin.cpp
@@ -0,0 +1,95 @@
+﻿// Tipovi podataka, varijable, cin
+/* U svim programskim jezicima, podacima manipulišemo tako što definišemo njihov tip i
+spašavamo ih u varijable, nizove ili objekte (o nizovima i objektima detaljnije u sljedećim materijalima).
+Programski jezik c++ ima nekoliko predefinisanih tipova podataka, ali osnovni i najkroišteniji su:
+int (integer) - cjelobrojne vrijednosti;
+float - decimalne vrijednosti;
+double - decimalne vrijednosti (veća preciznost u odnosu na float);
+char - karakteri definisani ascii kodom;
+bool - pamti samo dva stanja [True / False];
+void - tip podatka koji ne zahtjeva povratnu informaciju;
+[Za detaljnije informacije o svakom tipu podatka, proguglajte].
+
+Varijable 
+ - Tipovi podataka koji imaju:
+	- naziv
+	- adresu u memoriji
+	- definisan tip podatka
+	- vrijednost koju pamte
+*/
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int broj;
+	/* Deklarisanje varijable
+	Definisanje tipa podatka [int]
+	Davanje imena 'broj'
+	*/
+	int broj2 = 5;
+	/* Deklarisanje i inicijalizacija varijable
+	Definisanje tipa podatka [int]
+	Davanje imena 'broj2'
+	Dodjeljivanje vrijednosti '5'
+	*/
+	cout << "Unesite vrijednost broja: ";
+	cin >> broj;
+	/* Unos vrijednosti u varijablu broj
+	std::cin (iz standardnih komandi pozovi komandu cin) je komanda koja znači 'console-in'
+	i zadužena je za učitavanje podataka sa konzole.
+	Znakovi redirekcije >> dio su sintakse za komandu cin. Znakovima redirekcije možemo
+	razdvojiti unos pojedinačnih varijabli.*/
+
+	/* u ovoj liniji koda, varijabla broj2 ima vrijednost 5.
+	Varijabla broj će imati vrijednost unesenu sa tastature.*/
+	cout << "Broj: " << broj << " i broj2: " << broj2 << endl;
+	
+	system("pause>nul");
+
+	float realanBroj;
+	float realanBroj2 = 14.4f; //f znači float
+
+	// cout << realanBroj << " " << realanBroj2 << endl;
+
+	/* Ispis sadržaja varijabli 'realanBroj' i 'realanBroj2'
+	* Program se neće izvršiti jer pokušavamo da ispišemo sadržaj varijable koja nije
+	* inicijalizovana. 
+	*/
+	realanBroj = 1.02f;
+	cout << realanBroj << " " << realanBroj2 << endl;
+	/* Program će izvršiti ispis jer obe varijable sadržavaju neku vrijednost.
+	* Inicijalizaciju varijabli možemo raditi bilo gdje u kodu, jedini uslov je da je
+	* varijabla prije unosa ili inicijalizacije deklarisana.
+	*/
+	system("pause>nul");
+
+	int a, b;
+	/* Ukoliko želimo deklarisati više varijabli istog tipa podatka, možemo
+	i na ovaj način [razdvajanjem imena zarezom].*/
+
+	cout << "Unesite a i b: " << endl;
+	cin >> a >> b;
+
+	cout << "a: " << a << " / b: " << b << endl;
+	system("pause>nul");
+
+	char karakter;
+	cout << "Unesite karakter: "; 
+	cin >> karakter;
+	cout << (int)karakter << " - " << karakter;
+	/* linije 78 - 81
+	Deklarisali smo varijablu tipa char i dali joj ime 'karakter'. U sljedeće dvije linije
+	koda, unijeli smo karakter sa tastature. U liniji 81 ispisivamo ascii kod unesenog karaktera
+	i sam karakter. Karakteri su poseban tip podatka u svim programskim jezicima, tako da se
+	isti pamte po ascii tabeli koja definiše cjelobrojnu vrijednost za određeni karakter.
+	Ascii kod možemo saznati castingom (linija 81). Casting je način explicitne konverzije
+	određenog tipa podatka u drugi tip. [ (tip_podatka)naziv_varijable ].
+	U liniji 81 smo uradili casting nad varijablom karakter koja je tipa char, te nam je
+	program vratio integer vrijednost unijetog karaktera.
+	*/
+
+	system("pause>nul");
+	return 1;
+}
---
+++ First year/I Semester/PR I/Materijali/Uvod/uvod.cpp
@@ -0,0 +1,59 @@
+﻿// UVOD
+/* Programiranje je vještina kojom računaru kroz algoritme definišemo određenu funkcionalnost
+nekog koda. Proces programiranja zahtjeva strpljenje, upornost, trud, rješavanje logičkih problema i
+znanje. Kroz ovaj repozitorij proći ćemo kroz oblasti neophodne za savladavanje osnovna programiranja.
+
+Programski jezik u kojem ćemo raditi je C++. 
+Kroz ovaj program ćemo objasniti značenje svake linije koda i šta omogućava ovom kodu.*/
+
+#include<iostream>
+/* Aktiviranje biblioteke iostream.
+Ova biblioteka nam ogućava osnovne input i output komande na konzoli, koju ćemo koristiti za
+testiranje našeg koda. Ključna riječ 'include' se korsiti za aktiviranje biblioteka koje su dostupne
+programeru (definisanje ličnih biblioteka je moguće).*/
+
+using namespace std;
+/* Aktiviranje standardnih komandi unutar biblioteke.
+Iako možda nije najbolja praksa, na nivou učenja, tj. kreiranja programa koji koristi samo jedan
+korisnik, radi lakše implementacije cjelokupnog koda, neće izazvati nikakvu šetetu ova linija koda.
+Slučaj u kojem ne bismo trebali primjenjivati ovu liniju koda je kada radimo na većim projektima
+na kojem rade više programera, sa dosta različitih ličnih biblioteka gdje postoji mogućnost da su
+unutar drugih biblioteka definisane standardne komande sa istom ključnom riječju 'std'.
+Standardne komande su definisane unutar biblioteke.*/
+
+int main() {
+	/* Glavna/main funkcija.
+	Funkcija koja prva ulazi u stek memoriju i koja se prva pokreće.
+	Definiše se tipom podatka (obično je int ili void) i ključnom riječju 'main'.
+	Zagrade () definišu parametre koje funkcija prihvata (u ovom slučaju, ne proslijeđujemo ništa).
+	Zagrade {} definišu tijelo funkcije, tj. kod koji će biti izvršen unutar te funkcije.*/
+
+	cout << "Hello World!" << endl;
+	/* cout komanda - definisana unutar ostream biblioteke.
+	std::cout (iz standardnih komandi std, pozovi komandu cout) koja znači 'console-out' je komanda
+	zadužena za ispis na konzoli.
+	Znakovi redirekcije << su dio sintakse za komandu cout. Također, znakovima redirekcije možemo
+	razdvajati ispis texta, varijable ili povratni podatak neke funkcije.
+	Znakovi "" definišu početak i kraj textualnog zapisa koji će se pojaviti na konzoli. 
+	std::endl (iz standardnih komandi std, pozovi komandu endl) koja znači 'end line' je 
+	zadužena za prebacivanje u novi red nakon ispisa. Također se može koristiti i "\n" 
+	npr -> cout << "Hello World\n"; 
+	Kraj svake linije koda u c++ definiše znak ';' */
+
+	system("pause>null");
+	/* sistemski poziv
+	U c++ su omogućeni sistemski pozivi. Isti zaustavljaju izvršavanje koda, te izvršavaju
+	komandu sistemskog poziva. U ovom primjeru, pozvali smo komadnu za zaustavljanje konzole,
+	što znači da će se naš kod zaustaviti na liniji 42 i nastaviće sa izvršavanjem kada
+	korisnik pošalje neku interakciju.*/
+
+	return 1;
+	/* povratna vrijednost
+	Funkcija 'main' je definisana tipom 'int'(tip za cjelobrojne vrijednosti [brojevi iz skupa Z])
+	Svaka funckija mora da ima povratnu vrijednost (osim void funkcija), te u našem slučaju, 
+	main funkcija je dužna vratiti cjelobrojnu vrijednost (kod main funkcije definisane 'void' tipom podatka, 
+	ova linija nije obavezna). */
+}
+/* Zatvorena vitičasta zagrada '}'
+U c++ programskom jeziku, znak } definiše kraj tijela funkcije (u ovom slučaju main funkcije)
+i time se završava izvršavanje koda.*/
